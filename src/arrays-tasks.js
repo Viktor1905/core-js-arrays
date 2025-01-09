01958b677066ceb7a89b194bf55b49cd481c893e
Viktor1905 <webdevvva@gmail.com>
2025-01-09 21:32:41 +0300

    feat: done some more

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -653,8 +653,17 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let result;
+  let subArr;
+  if (n > 0) {
+    subArr = arr.splice(arr.length - n, n);
+    result = subArr.concat(arr);
+  } else {
+    subArr = arr.splice(0, -n);
+    result = arr.concat(subArr);
+  }
+  return result;
 }
 
 /**
